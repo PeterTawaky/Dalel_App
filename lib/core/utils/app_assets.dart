13ffd938b385515385ab4ab156ae1540a4b2d670
Peter Ayman <peterayman800@gmail.com>
2025-10-07 18:01:00 +0300

    last update

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,10 @@
+class AppAssets {
+  AppAssets._();
+  static const onboarding1 = 'assets/images/onboarding1.svg';
+  static const onboarding2 = 'assets/images/onboarding2.svg';
+  static const onboarding3 = 'assets/images/onboarding3.svg';
+  static const forgetPassword = 'assets/images/forget_password.svg';
+  static const orderSuccess = 'assets/images/order_success.svg';
+  static const vector1 = 'assets/images/vector1.svg';
+  static const vector2 = 'assets/images/vector2.svg';
+}
